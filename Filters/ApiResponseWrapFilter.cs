c019610916e348623bd100cdcffc749b52c6f8e0
edward <jifengming92@163.com>
2026-01-09 09:17:01 +0800

    增加自定义包头的跳过，以及文件下载时自定义包的跳过

--- Filters/ApiResponseWrapFilter.cs
+++ Filters/ApiResponseWrapFilter.cs
@@ -11,6 +11,26 @@ public class ApiResponseWrapFilter : IAsyncResultFilter
 {
     public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
     {
+        var response = context.HttpContext.Response;
+
+        // ① 如果响应已经开始写，就不能再包装
+        if (response.HasStarted) { await next(); return; }
+
+        // ② 如果是文件下载，自动跳过包装
+        var contentType = response.ContentType?.ToLower();
+
+        if (contentType != null &&
+            (contentType.Contains("application/gzip") ||
+            contentType.Contains("application/octet-stream") ||
+            contentType.Contains("application/pdf") ||
+            contentType.Contains("application/zip") ||
+            contentType.Contains("text/csv")))
+        { await next(); return; }
+
+        // ③ 如果返回的是 FileResult，也跳过包装
+        if (context.Result is FileResult) { await next(); return; }
+
+
         // 如果已经是 ApiResponse，不再包装
         if (context.Result is ObjectResult objectResult)
         {
